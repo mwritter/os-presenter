9bcd5ce2c5fd08a809a952f530074b9c29a668c3
Matthew Ritter <matthewwilliamritter@gmail.com>
2025-11-23 00:00:17 -0500

    Updated popover and context menu base styles

--- src/components/presenter/show/ShowViewFooter.tsx
+++ src/components/presenter/show/ShowViewFooter.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { PopoverArrow } from "@radix-ui/react-popover";
 import { Plus } from "lucide-react";
 
 export const ShowViewFooter = ({
@@ -8,14 +14,24 @@ export const ShowViewFooter = ({
 }) => {
   return (
     <div className="flex w-full h-10 items-center justify-between bg-shade-3 px-2 sticky bottom-0 left-0 right-0">
-      <Button
-        className="text-gray-400 rounded-sm hover:text-gray-400 hover:bg-white/10"
-        variant="ghost"
-        size="icon-xs"
-        onClick={onAddBlankSlide}
-      >
-        <Plus className="size-3" />
-      </Button>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="ghost" size="icon-xs">
+            <Plus className="size-3" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-2">
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={onAddBlankSlide}
+            className="w-full justify-start text-xs hover:bg-white/10!"
+          >
+            Add New Blank Slide
+          </Button>
+          <PopoverArrow className="fill-shade-1" />
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
